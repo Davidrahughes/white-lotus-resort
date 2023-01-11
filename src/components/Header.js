f1c6e382c423f9c3239ea667fd6ca8ed47ca2a50
Dave Hughes <hughesd11@gmail.com>
2023-01-10 20:32:12 -0500

    Linked Firebase info to React

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+  // console.log('Header has rendered');
+  return (
+    <header>
+      <h1>White Lotus Welcomes You</h1>
+    </header>
+  );
+};
+
+export default Header;
